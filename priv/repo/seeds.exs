479715e22aa61421a52488db789a4eba5b2f4866
Gustavo Oliveira <g.92oliveira@gmail.com>
2020-12-26 09:16:42 -0300

    build script

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,30 +1,39 @@
+defmodule Insert do
+  def build_offers(offers) do
+    offers
+    |> Enum.group_by(& &1["university"])
+    |> controi_campos()
+  end
+
+  def controi_campos(universidades) do
+    universidades
+    |> Enum.map(fn {university, campus} ->
+      campus =
+        campus
+        |> Enum.map(&Map.delete(&1, "university"))
+        |> Enum.group_by(& &1["campus"])
+        |> Enum.map(&constroi_cursos/1)
+
+      Map.put(university, "campus", campus)
+    end)
+  end
+
+  def constroi_cursos({campus, cursos}) do
+    cursos = cursos
+    |> Enum.map(fn curso ->
+      curso = Map.delete(curso, "campus")
+
+      new_course = curso["course"]
+      oferta = curso |> Map.delete("course")
+      new_course
+      # curso = Map.put(new_course, "scolarships", [oferta])
+    end)
+    Map.put(campus, "courses", cursos)
+  end
+end
+
 File.read!("db.json")
 |> Jason.decode!()
-|> Enum.group_by(fn s ->
-  s["university"]
-end)
-|> Enum.map(fn {university, campus} ->
-  campus =
-    campus
-    |> Enum.map(&Map.delete(&1, "university"))
-    |> Enum.group_by(& &1["campus"])
-    |> Enum.map(fn {campus, curso} ->
-      curso =
-        curso
-        |> Enum.map(fn curso ->
-          curso = Map.delete(curso, "campus")
-
-          new_course = curso["course"]
-          oferta = curso |> Map.delete("course")
-          new_course
-          # curso = Map.put(new_course, "scolarships", [oferta])
-        end)
-
-      campus
-      |> Map.put("courses", curso)
-    end)
-
-  Map.put(university, "campus", campus)
-end)
+|> Insert.build_offers()
 |> Enum.each(&OffersCourse.Services.ScholarShips.execute/1)
 |> IO.inspect()
